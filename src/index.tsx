f1263fb7052d64a7340e5d640f09b6196ac28749
teunlao <teunlao@gmail.com>
2021-01-17 00:56:26 +0300

    chore: prototype for tranpile jsx files in the browser

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import * as esbuild from 'esbuild-wasm';
+import React, { useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015',
+    });
+
+    console.log(result);
+    setCode(result.code);
+  };
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
   return (
-    <h1>
-      Hello2222222222222222 312312312312312312312312312312 222222222222222222222222222222222222222222222222e ew qweqw
-      eqwe qweqwe qw
-    </h1>
+    <div>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)} />
+      <div>
+        <button type="submit" onClick={onClick}>
+          Submit
+        </button>
+      </div>
+      <pre>{code}</pre>
+    </div>
   );
 };
 
