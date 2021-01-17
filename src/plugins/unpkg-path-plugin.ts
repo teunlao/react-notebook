f9f9a126356218ab21e60fe9cd5df258103249ee
teunlao <teunlao@gmail.com>
2021-01-17 04:58:39 +0300

    feat: add possibility to bundle dynamic modules

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,4 +1,5 @@
 import * as esbuild from 'esbuild-wasm';
+import axios from 'axios';
 
 // eslint-disable-next-line import/prefer-default-export
 export const unpkgPathPlugin = (): esbuild.Plugin => {
@@ -7,7 +8,28 @@ export const unpkgPathPlugin = (): esbuild.Plugin => {
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         console.log('onResolve', args);
-        return { path: args.path, namespace: 'a' };
+        if (args.path === 'index.js') {
+          return { path: args.path, namespace: 'a' };
+        }
+
+        if (args.path.includes('./') || args.path.includes('../')) {
+          return {
+            namespace: 'a',
+            path: new URL(args.path, `https://unpkg.com/${args.resolveDir}/`).href,
+          };
+        }
+
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`,
+        };
+
+        // else if (args.path === 'tiny-test-pkg') {
+        //   return {
+        //     path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
+        //     namespace: 'a',
+        //   };
+        // }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -17,11 +39,19 @@ export const unpkgPathPlugin = (): esbuild.Plugin => {
           return {
             loader: 'jsx',
             contents: `
+              const message = require('react-dom');
               console.log(message);
             `,
           };
         }
-        return null;
+
+        const { data, request } = await axios.get(args.path);
+        console.log(request);
+        return {
+          loader: 'jsx',
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
       });
     },
   };
