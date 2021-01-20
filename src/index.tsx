e58bb36cdc1471912969484f5ff6c8029360f0fa
teunlao <teunlao@gmail.com>
2021-01-20 03:07:46 +0300

    refactor: move the bundle logic code into a separate module

--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,16 @@
-import * as esbuild from 'esbuild-wasm';
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import unpkgPathPlugin from './plugins/unpkg-path-plugin';
-import fetchPlugin from './plugins/fetch-plugin';
 import CodeEditor from './components/code-editor/code-editor';
 import Preview from './components/preview/preview';
+import bundle from './bundler';
 
 const App = () => {
-  const ref = useRef<any>();
   const [code, setCode] = useState('');
   const [input, setInput] = useState('');
 
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    });
-  };
-  useEffect(() => {
-    startService();
-  }, []);
-
   const onClick = async () => {
-    if (!ref.current) {
-      return;
-    }
-
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window',
-      },
-    });
-
-    setCode(result.outputFiles[0].text);
+    const output = await bundle(input);
+    setCode(output);
   };
 
   return (
