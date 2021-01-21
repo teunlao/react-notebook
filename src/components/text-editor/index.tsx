d8b2567b4dc80e997ad92262dc05d5b958d4586b
teunlao <teunlao@gmail.com>
2021-01-22 00:37:53 +0300

    feat: close text editor by click at outside

--- src/components/text-editor/index.tsx
+++ src/components/text-editor/index.tsx
@@ -1,10 +1,37 @@
 import MDEditor from '@uiw/react-md-editor';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const TextEditor: React.FC = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [editing, setEditing] = useState(false);
+
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (ref.current && event.target && ref.current.contains(event.target as Node)) {
+        return;
+      }
+
+      setEditing(false);
+    };
+
+    window.addEventListener('click', listener, { capture: true });
+
+    return () => {
+      document.removeEventListener('click', listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div ref={ref}>
+        <MDEditor />
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <MDEditor />
+    <div onClick={() => setEditing(true)}>
+      <MDEditor.Markdown source={'# Header'} />
     </div>
   );
 };
