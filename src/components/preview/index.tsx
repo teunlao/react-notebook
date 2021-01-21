dd093d96abea7907e41c39454c73ffd4df317266
teunlao <teunlao@gmail.com>
2021-01-21 23:54:48 +0300

    feat: handle a async errors

--- src/components/preview/index.tsx
+++ src/components/preview/index.tsx
@@ -13,13 +13,21 @@ const html = `
       <body>
         <div id="root"></div>
         <script>
-          window.addEventListener('message', (event) => {
+          const handleError = err => {
+            const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);
+          }
+          
+          window.addEventListener('error', event => {
+            handleError(event.error)
+          })
+        
+          window.addEventListener('message', event => {
             try {
               eval(event.data);
             } catch (err) {
-              const root = document.querySelector('#root');
-              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-              console.error(err);
+              handleError(err)
             }
           }, false);
         </script>
