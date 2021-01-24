086ffdcfb117737a586218c6ec47ec4a9ab15b8f
teunlao <teunlao@gmail.com>
2021-01-24 19:04:04 +0300

    feat: add styles for dark-theme md-editor

--- src/components/text-editor/index.tsx
+++ src/components/text-editor/index.tsx
@@ -24,7 +24,7 @@ const TextEditor: React.FC = () => {
 
   if (editing) {
     return (
-      <div ref={ref}>
+      <div className="text-editor" ref={ref}>
         <MDEditor />
       </div>
     );
