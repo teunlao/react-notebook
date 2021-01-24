67d2f7adf23809a78175e294b2a0fb4739bf59a5
teunlao <teunlao@gmail.com>
2021-01-24 19:09:38 +0300

    feat: add state to text-editor component

--- src/components/text-editor/index.tsx
+++ src/components/text-editor/index.tsx
@@ -5,6 +5,7 @@ import './text-editor.css';
 const TextEditor: React.FC = () => {
   const ref = useRef<HTMLDivElement | null>(null);
   const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState('# Header');
 
   useEffect(() => {
     const listener = (event: MouseEvent) => {
@@ -25,14 +26,14 @@ const TextEditor: React.FC = () => {
   if (editing) {
     return (
       <div className="text-editor" ref={ref}>
-        <MDEditor />
+        <MDEditor value={value} onChange={(v) => setValue(v || '')} />
       </div>
     );
   }
 
   return (
     <div onClick={() => setEditing(true)}>
-      <MDEditor.Markdown source={'# Header'} />
+      <MDEditor.Markdown source={value} />
     </div>
   );
 };
