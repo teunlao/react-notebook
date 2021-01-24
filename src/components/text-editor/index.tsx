2810fa82623f6cbe1917633182a726d1d9565627
teunlao <teunlao@gmail.com>
2021-01-24 19:12:47 +0300

    chore: styling text-editor preview

--- src/components/text-editor/index.tsx
+++ src/components/text-editor/index.tsx
@@ -25,15 +25,17 @@ const TextEditor: React.FC = () => {
 
   if (editing) {
     return (
-      <div className="text-editor" ref={ref}>
+      <div className="text-editor card" ref={ref}>
         <MDEditor value={value} onChange={(v) => setValue(v || '')} />
       </div>
     );
   }
 
   return (
-    <div onClick={() => setEditing(true)}>
-      <MDEditor.Markdown source={value} />
+    <div className="text-editor card" onClick={() => setEditing(true)}>
+      <div className="card-content">
+        <MDEditor.Markdown source={value} />
+      </div>
     </div>
   );
 };
